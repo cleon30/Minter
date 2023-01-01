4831ee00564e79c0f12ec16fd9460c3558da2a96
C <caaarloos300@gmail.com>
2023-01-02 02:17:53 +0900

    Update minter.ts

--- minter.ts
+++ minter.ts
@@ -1,5 +1,5 @@
-import {keypairIdentity, Metaplex} from "@metaplex-foundation/js";
-import {clusterApiUrl, Connection, Keypair} from "@solana/web3.js";
+import {keypairIdentity, Metaplex,MintAuthorityMustBeSignerToMintInitialSupplyError,mintCandyMachineV2Operation,toBigNumber} from "@metaplex-foundation/js";
+import {clusterApiUrl, Connection, Keypair, PublicKey} from "@solana/web3.js";
 const fs = require("fs");
 const anchor = require("@project-serum/anchor");
 const connection = new Connection(clusterApiUrl("devnet")); 
@@ -13,15 +13,41 @@ async function main(){
 const collectionAuthority = loadKeypair("minf8m9eFyp7292L77V9yfTYEgUoDW476ZkqoXng9Bi.json");
 metaplex.use(keypairIdentity(collectionAuthority));
 // CandyMachine configuration 
-const { nft: collectionNft } = await metaplex.nfts().create({
-    name: "My Collection NFT",
-    uri: "https://shdw-drive.genesysgo.net/CL4s5cFeeRe4tc4ihrPkNpbqWNVykKyZRtHvdwTSWDnH/orca.json",
-    sellerFeeBasisPoints: 0,
-    isCollection: true,
-    updateAuthority: collectionAuthority,
+// const { nft: collectionNft } = await metaplex.nfts().create({
+//     name: "My Collection NFT",
+//     uri: "https://shdw-drive.genesysgo.net/CL4s5cFeeRe4tc4ihrPkNpbqWNVykKyZRtHvdwTSWDnH/orca.json",
+//     sellerFeeBasisPoints: 0,
+//     isCollection: true,
+//     updateAuthority: collectionAuthority,
+// });
+const collectionNft = new PublicKey("BrJH63SoYNHYGAWA2FDGUkctkKaVi3xAosnc4URuoWVr");
+// console.log(collectionNft.address.toBase58());
+const { candyMachine } = await metaplex.candyMachines().create({
+  itemsAvailable: toBigNumber(100),
+  sellerFeeBasisPoints: 333, // 3.33%
+  collection: {
+    address: collectionNft,
+    updateAuthority: metaplex.identity(),
+  },
+  isMutable : true,
+   symbol:"PROJECT",
+   maxEditionSupply:toBigNumber(0),
+   creators:[
+     {
+       address: collectionAuthority.publicKey,
+       share:100
+     }
+   ],
+   itemSettings:{
+     type:"configLines",
+     prefixName:"Orca Number #$ID+1$",
+     nameLength:0,
+     prefixUri:"https://shdw-drive.genesysgo.net/CL4s5cFeeRe4tc4ihrPkNpbqWNVykKyZRtHvdwTSWDnH/$ID$.json",
+     uriLength:43,
+     isSequential:false,
+   },
+  
 });
-console.log(collectionNft.address.toBase58());
-
 };
 
 main();
